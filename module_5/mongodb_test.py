c5a614ee0f2456b7d4542dcb3472f5c85b4c7d76
KMCAIN <54560734+KMCAIN@users.noreply.github.com>
2021-04-11 16:56:31 -0500

    added file pytech_insert and pytech_quiries and also updated mongodb_test

--- module_5/mongodb_test.py
+++ module_5/mongodb_test.py
@@ -1,6 +1,13 @@
+'''
+Kaymon McCain
+Assignment 5.2
+4/11/2021
+'''
+import pymongo
 from pymongo import MongoClient
 
 url = "mongodb+srv://admin:admin@cluster0.f2jka.mongodb.net/pytech?retryWrites=true&w=majority"
 client = MongoClient(url)
-db = client.pytech
-print(db.list_collection_names)
+db = client["pytech"]
+collectionNames = db.list_collection_names()
+print(collectionNames)
