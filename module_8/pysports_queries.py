185a7340f80254cc257cd7a5a821165ea1c0d7de
KMCAIN <54560734+KMCAIN@users.noreply.github.com>
2021-04-25 19:59:34 -0500

    Made some updates to pysports_queries.py

--- module_8/pysports_queries.py
+++ module_8/pysports_queries.py
@@ -7,8 +7,6 @@ try:
     config = {"host": "localhost", "user": "root", "password": "ShowMeTheShadow402!",
               "database": "pysports", "raise_on_warnings": True}
     mydb = mysql.connector.connect(**config)
-    print("\n Database user {} connected MySQL on host {} with database {}.".format(
-        config["user"], config["host"], config["database"]))
 except mysql.connector.Error as err:
     if err.errno == errorcode.ER_KEYRING_ACCESS_DENIED_ERROR:
         print("    The supplied username or password are invalid")
