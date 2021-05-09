dd492a21e0fc946d54d76c03d08ef9bf3a68ef5c
KMCAIN <54560734+KMCAIN@users.noreply.github.com>
2021-05-09 08:11:33 -0500

    Make User in Script

--- module_10/whatABookScript.sql
+++ module_10/whatABookScript.sql
@@ -1,5 +1,14 @@
 use whatabook;
 
+-- drop test user if exists 
+DROP USER IF EXISTS 'whatabook_user'@'localhost';
+
+-- create whatabook_user and grant them all privileges to the whatabook database 
+CREATE USER 'whatabook_user'@'localhost' IDENTIFIED WITH mysql_native_password BY 'MySQL8IsGreat!';
+
+-- grant all privileges to the whatabook database to user whatabook_user on localhost 
+GRANT ALL PRIVILEGES ON whatabook.* TO'whatabook_user'@'localhost';
+
 DROP TABLE IF EXISTS store;
 DROP TABLE IF EXISTS wishlist;
 DROP TABLE IF EXISTS book;
